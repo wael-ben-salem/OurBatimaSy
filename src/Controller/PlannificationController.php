5f52769b64073028d55798a03cb4772b999ec142
lmahdy <mahdi.mzoughi@istic.ucar.tn>
2025-04-17 11:42:44 +0100

    discussion:)

--- src/Controller/PlannificationController.php
+++ src/Controller/PlannificationController.php
@@ -11,6 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// Add these use statements
+use App\Entity\Discussion;
+use App\Form\DiscussionType;
+
 #[Route('/plannification')]
 class PlannificationController extends AbstractController
 {
@@ -46,11 +50,36 @@ class PlannificationController extends AbstractController
         ]);
     }
 
-    #[Route('/{idPlannification}', name: 'app_plannification_show', methods: ['GET'])]
-    public function show(Plannification $plannification): Response
+    #[Route('/{idPlannification}', name: 'app_plannification_show', methods: ['GET', 'POST'])]
+    public function show(Plannification $plannification, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $discussion = new Discussion();
+        $form = $this->createForm(DiscussionType::class, $discussion);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $discussion->setPlannification($plannification);
+            $discussion->setSender($this->getUser());
+            $discussion->setRecipient($plannification->getIdTache()->getArtisan()->getArtisan());
+            $discussion->setCreatedAt(new \DateTime());
+
+            $entityManager->persist($discussion);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_plannification_show', [
+                'idPlannification' => $plannification->getIdPlannification()
+            ]);
+        }
+
+        $messages = $entityManager->getRepository(Discussion::class)->findBy(
+            ['plannification' => $plannification],
+            ['createdAt' => 'ASC']
+        );
+
         return $this->render('plannification/show.html.twig', [
             'plannification' => $plannification,
+            'messages' => $messages,
+            'form' => $form->createView()
         ]);
     }
 
